8d2c0a57ef0cbc90653be6e1a442a35735e6c8f4
VladDrobitko <boss.drobitko@gmail.com>
2024-08-04 19:07:38 +0200

    Initial Commit

---
+++ WordScramble/WordScrambleApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordScrambleApp.swift
+//  WordScramble
+//
+//  Created by Владислав Дробитько on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct WordScrambleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
